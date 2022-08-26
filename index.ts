69d1b8b37f83540a5f1ee9429725827cba02a846
Stanislav Stehniy <sstehniy@gmail.com>
2022-08-26 16:10:59 +0200

    improve

--- index.ts
+++ index.ts
@@ -1,13 +1,51 @@
 import { google } from "googleapis";
-import { Context, Telegraf } from "telegraf";
+import Redis from "ioredis";
+import { ScheduledTask } from "node-cron";
+import { Telegraf } from "telegraf";
+import * as cron from "node-cron";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const youtube = google.youtube("v3");
-import * as cron from "node-cron";
+
+const redis = new Redis({
+  host: "redis-16144.c89.us-east-1-3.ec2.cloud.redislabs.com",
+  port: 16144,
+  password: process.env.REDIS_PW,
+});
+
+let tasks: { chatId: string; task: ScheduledTask }[] = [];
 
 const playlists = [
   "PLnBhm4gyqDGAV-BESRro-tQ5JDhWuV8SE",
   "PLnBhm4gyqDGD9CmfYrs4NZWZcQK-ufle7",
 ];
+
+const startCron = (chatId: string) => {
+  const task = cron.schedule("0 7 * * *", async () => {
+    const links = await getRandomVideos();
+    links.forEach((link, idx) => {
+      console.log(`${link.link.split("v=")[1]}-${link.playlistId}-${idx + 1}`);
+      bot.telegram.sendMessage(chatId, `${idx + 1}#: ${link.link}`, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Другое Видео",
+                callback_data: `${link.link.split("v=")[1]}_${
+                  link.playlistId
+                }_${idx + 1}`,
+              },
+            ],
+          ],
+        },
+      });
+    });
+  });
+  task.start();
+  return task;
+};
 
 const getRandomVideoFromPlayList = async (
   playlistId: string,
@@ -41,15 +79,6 @@ const getRandomVideoFromPlayList = async (
 };
 
 const getRandomVideos = async () => {
-  // const randomIdFromFirstPlaylist = (await new Promise((resolve, reject) => {
-
-  //     }
-  //   );
-  // })) as string;
-  // const randomIdFromSecondPlaylist = (await new Promise((resolve, reject) => {
-  //
-  // })) as string;
-
   const ids = (await Promise.all(
     playlists.map(async (pl) => {
       return await new Promise((resolve, reject) => {
@@ -83,47 +112,27 @@ const getRandomVideos = async () => {
   }));
 };
 
-const startCron = (ctx: Context) => {
-  const task = cron.schedule("0 7 * * *", async () => {
-    const links = await getRandomVideos();
-    links.forEach((link, idx) => {
-      console.log(
-        `${link.link.split("v=")[1]}-${link.playlistId}-${
-          ctx.message.message_id
-        }-${idx + 1}`
-      );
-      ctx.reply(`${idx + 1}#: ${link.link}`, {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: "Другое Видео",
-                callback_data: `${link.link.split("v=")[1]}_${
-                  link.playlistId
-                }_${ctx.message.message_id}_${idx + 1}`,
-              },
-            ],
-          ],
-        },
-      });
-    });
-  });
-  task.start();
-};
-
 const bot = new Telegraf("5532085065:AAGH1oSLNazNn02pnmG2-jWKM9C1gvqFl8M");
 
 bot.start(async (ctx) => {
-  startCron(ctx);
-
+  // await fetch("http://localhost:3000/schedule-job", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(ctx),
+  // });
+  const cacheResponse = await redis.get(ctx.chat.id.toString());
+  if (!cacheResponse) {
+    await redis.set(ctx.chat.id.toString(), 1);
+    tasks.push({
+      chatId: ctx.chat.id.toString(),
+      task: startCron(ctx.chat.id.toString()),
+    });
+  }
   ctx.reply("Привет, любимая. Надеюсь, это сработает...");
   const links = await getRandomVideos();
   links.forEach((link, idx) => {
-    console.log(
-      `${link.link.split("v=")[1]}-${link.playlistId}-${
-        ctx.message.message_id
-      }-${idx + 1}`
-    );
     ctx.reply(`${idx + 1}#: ${link.link}`, {
       reply_markup: {
         inline_keyboard: [
@@ -141,32 +150,29 @@ bot.start(async (ctx) => {
   });
 });
 
-bot.action(
-  /([a-zA-Z0-9_]+)[_]([a-zA-Z0-9-]+)[_]([0-9]+)[_]([0-9]+)/,
-  async (ctx) => {
-    console.log("here");
-    const videoId = ctx.match[1];
-    const playlistId = ctx.match[2];
-    const messageId = ctx.match[3];
-    const videoIdx = ctx.match[4];
-    console.log(playlistId, videoId);
-    const videoLink = await getRandomVideoFromPlayList(playlistId, videoId);
-    ctx.reply(`${+videoIdx}#: ${videoLink.link}`, {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "Другое Видео",
-              callback_data: `${videoLink.link.split("v=")[1]}_${
-                videoLink.playlistId
-              }_${messageId}_${videoIdx}`,
-            },
-          ],
+bot.action(/([a-zA-Z0-9_]+)[_]([a-zA-Z0-9-]+)[_]([0-9]+)/, async (ctx) => {
+  console.log("here");
+
+  const videoId = ctx.match[1];
+  const playlistId = ctx.match[2];
+  const videoIdx = ctx.match[3];
+
+  const videoLink = await getRandomVideoFromPlayList(playlistId, videoId);
+  ctx.reply(`${+videoIdx}#: ${videoLink.link}`, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Другое Видео",
+            callback_data: `${videoLink.link.split("v=")[1]}_${
+              videoLink.playlistId
+            }_${videoIdx}`,
+          },
         ],
-      },
-    });
-  }
-);
+      ],
+    },
+  });
+});
 
 bot.command("refresh", async (ctx) => {
   const links = await getRandomVideos();
@@ -190,7 +196,26 @@ bot.command("refresh", async (ctx) => {
     });
   });
 });
-bot.launch();
+bot.command("quit", async (ctx) => {
+  // Using context shortcut
+  await redis.del(ctx.chat.id.toString());
+  const taskToStop = tasks.find((t) => t.chatId !== ctx.chat.id.toString());
+  taskToStop.task.stop();
+  tasks = tasks.filter((t) => t.chatId !== ctx.chat.id.toString());
+  ctx.leaveChat();
+});
+
+bot.launch().then(async () => {
+  const allKeys = await redis.keys("*");
+  console.log(allKeys);
+  await Promise.all(
+    allKeys.map(async (key) => {
+      if (!tasks.find((t) => t.chatId === key)) {
+        tasks.push({ chatId: key, task: startCron(key) });
+      }
+    })
+  );
+});
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
